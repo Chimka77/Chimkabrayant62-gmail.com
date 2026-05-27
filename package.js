d430625cac654b0ff2a422608e39cf983ba68a32
Chimka77 <chimkabrayant62@gmail.com>
2026-05-27 01:39:16 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "goldhunt-referral-backend",
+  "version": "1.0.0",
+  "description": "Secure referral system backend for GoldHunt Telegram Mini App",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "firebase-admin": "^12.0.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.3"
+  }
+}
